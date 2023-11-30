f8d0d11ea531ff09be6628df90b24e470f4b89a4
ichiro-ss <sei.swmr@gmail.com>
2023-12-01 03:51:39 +0900

    added thread display

--- src/Threads.jsx
+++ src/Threads.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Threads = () => {
+  //   const [threadUrl, setThreadUrl] = useState(
+  //     "https://railway.bulletinboard.techtrain.dev/threads"
+  //   );
 
-    return <></>
-}
+  const [threadTitles, setThreadTitles] = useState([""]);
+  let threadIds = [];
+  let threadTitleStrs = [];
 
-export default Threads
+  useEffect(() => {
+    getThreads();
+  }, []);
+
+  async function getThreads() {
+    const res = await fetch(
+      "https://railway.bulletinboard.techtrain.dev/threads"
+    );
+    const data = await res.json();
+    for (let i = 0; i < data.length; i++) {
+      threadIds.push(data[i].id);
+      threadTitleStrs.push(data[i].title);
+    }
+    setThreadTitles(threadTitleStrs);
+  }
+  const threadList = threadTitles.map((thread) => <li>{thread}</li>);
+
+  return <ul>{threadList}</ul>;
+};
+
+export default Threads;
